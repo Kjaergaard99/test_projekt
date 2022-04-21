8b08df246ea8cf22925e527463e6d4c11e32e685
danieldawsonjensen <dani540c@gmail.com>
2022-04-21 10:54:17 +0200

    properties

--- test_projekt/Shared/Models/Shelter.cs
+++ test_projekt/Shared/Models/Shelter.cs
@@ -4,41 +4,22 @@ using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson;
 using System.ComponentModel.DataAnnotations;
 
+
 namespace test_projekt.Shared.Models
 {
+	[BsonIgnoreExtraElements]
+
 	public class Shelter
 	{
-		[Required]
-		[BsonElement("shelter_id")]
-		public int ShelterId { get; set; }
 
-		[Required]
-		[BsonElement("navn")]
-		public string Navn { get; set; }
-
-		[Required]
-		[BsonElement("beskrivels")]
-		public string Beskrivelse { get; set; }
-
-		[Required]
-		[BsonElement("antal_pl")]
-		public int AntalPladser { get; set; }
-
-		[Required]
-		[BsonElement("postnr")]
-		public int PostNr { get; set; }
-
-		[Required]
-		[BsonElement("vejnavn")]
-		public string Vejnavn { get; set; }
-
-		[Required]
-		[BsonElement("husnr")]
-		public string HusNr { get; set; }
+		
 
 		[BsonId]
 		[BsonRepresentation(BsonType.ObjectId)]
 		public string? MongoId { get; set; }
+
+		[BsonElement("properties")]
+		public ShelterProperty Property { get; set; }
 
 		public Shelter()
 		{
---
+++ test_projekt/Shared/Models/ShelterProperty.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System.ComponentModel.DataAnnotations;
+
+namespace test_projekt.Shared.Models
+{
+	[BsonIgnoreExtraElements]
+	public class ShelterProperty
+	{
+		public ShelterProperty()
+		{
+		}
+
+		[Required]
+		[BsonElement("shelter_id")]
+		public int ShelterId { get; set; }
+
+		[Required]
+		[BsonElement("navn")]
+		public string Navn { get; set; }
+
+		[Required]
+		[BsonElement("beskrivels")]
+		public string Beskrivelse { get; set; }
+
+		[Required]
+		[BsonElement("antal_pl")]
+		public int AntalPladser { get; set; }
+
+		[Required]
+		[BsonElement("postnr")]
+		public int PostNr { get; set; }
+
+		[Required]
+		[BsonElement("vejnavn")]
+		public string Vejnavn { get; set; }
+
+		[Required]
+		[BsonElement("husnr")]
+		public string HusNr { get; set; }
+	}
+}
+
