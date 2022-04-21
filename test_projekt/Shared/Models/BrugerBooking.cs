741d1ef5aa0a8a164cd40b06f541ab2200003fe4
danieldawsonjensen <dani540c@gmail.com>
2022-04-21 13:22:59 +0200

    oprettelse af booking labels osv

---
+++ test_projekt/Shared/Models/BrugerBooking.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace test_projekt.Shared.Models
+{
+	public class BrugerBooking
+	{
+		public string Navn { get; set; }
+
+		public string Email { get; set; }
+
+		public DateTime StartDato { get; set; } = DateTime.Now;
+
+		public DateTime SlutDato { get; set; } = DateTime.Now;
+
+
+		public BrugerBooking(string navn = "", string email = "")
+		{
+			this.Navn = navn;
+			this.Email = email;
+		}
+	}
+}
+
